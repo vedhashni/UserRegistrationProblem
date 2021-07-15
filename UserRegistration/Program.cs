c2ebe60283119a50e03c4ce9aec18e322621cf9a
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-15 20:03:20 +0530

    [Vedhashni] Updated the file with Initial Commit

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to validating User Registration using Regular Expression");
+        }
+    }
+}
